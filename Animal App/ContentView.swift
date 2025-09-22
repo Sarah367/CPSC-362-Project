21965622f341ef37e01c9a95208db4d6c0a4f58e
Sarah <komalagn2@csu.fullerton.edu>
2025-09-22 11:27:47 -0700

    Initial Commit

---
+++ Animal App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animal App
+//
+//  Created by Andrea Maples on 9/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Animal AppTests/Animal_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_AppTests.swift
+//  Animal AppTests
+//
+//  Created by Andrea Maples on 9/22/25.
+//
+
+import Testing
+@testable import Animal_App
+
+struct Animal_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
